8040038ee2ff47e798042093d7adcec4c46e5cd7
zakariamagdyz <zakariamagdy@ymail.com>
2023-08-12 21:24:56 +0300

    feat: enhance skeleton

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,7 +3,7 @@ import Skeleton from "@/components/ui/skeleton";
 const Loading = () => {
   return (
     <main className="container">
-      <div className="h-full w-full p-8">
+      <section className="h-full w-full p-8">
         <Skeleton className="aspect-square w-full rounded-xl md:aspect-[2.4/1]" />
         <div className="mt-8 grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
           <Skeleton className="aspect-square rounded-xl" />
@@ -13,7 +13,7 @@ const Loading = () => {
           <Skeleton className="aspect-square rounded-xl" />
           <Skeleton className="aspect-square rounded-xl" />
         </div>
-      </div>
+      </section>
     </main>
   );
 };
